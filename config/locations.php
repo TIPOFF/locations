ceac2848803a4e007e7c975593c892fa0e223f20
Phuc Le <phuclh93@gmail.com>
2021-02-04 17:21:03 -0800

    Remove all model references and rename service to service_class

--- config/locations.php
+++ config/locations.php
@@ -2,45 +2,7 @@
 
 return [
 
-    'model_class' => [
-
-        'user' => \App\Models\User::class,
-
-        'contact' => \App\Models\Contact::class,
-
-        'tax' => \App\Models\Tax::class,
-
-        'fee' => \App\Models\Fee::class,
-
-        'order' => \App\Models\Order::class,
-
-        'review' => \App\Models\Review::class,
-
-        'insight' => \App\Models\Insight::class,
-
-        'feedback' => \App\Models\Feedback::class,
-
-        'room' => \App\Models\Room::class,
-
-        'signature' => \App\Models\Signature::class,
-
-        'competitor' => \App\Models\Competitor::class,
-
-        'snapshot' => \App\Models\Snapshot::class,
-
-        'slot' => \App\Models\Slot::class,
-
-        'booking' => \App\Models\Booking::class,
-
-        'theme' => \App\Models\Theme::class,
-
-        'image' => \DrewRoberts\Media\Models\Image::class, // https://github.com/drewroberts/media
-
-        'video' => \DrewRoberts\Media\Models\Video::class, // https://github.com/drewroberts/media
-
-    ],
-
-    'service' => [
+    'service_class' => [
 
         'calendar' => \App\Services\CalendarService::class
 
