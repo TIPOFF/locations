db57dffb1606c6587cc2b8ae258be2cef55df343
drewroberts <drewroberts@users.noreply.github.com>
2021-02-12 21:16:23 +0000

    Fix styling

--- src/LocationsServiceProvider.php
+++ src/LocationsServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Locations;
 
-use Tipoff\Bookings\Models\Booking;
-use Tipoff\Bookings\Policies\BookingPolicy;
 use Tipoff\Locations\Models\Location;
 use Tipoff\Locations\Models\Market;
 use Tipoff\Locations\Policies\LocationPolicy;
@@ -18,7 +16,7 @@ class LocationsServiceProvider extends TipoffServiceProvider
         $package
             ->hasPolicies([
                 Location::class => LocationPolicy::class,
-                Market::class => MarketPolicy::class
+                Market::class => MarketPolicy::class,
             ])
             ->name('locations')
             ->hasConfigFile();
