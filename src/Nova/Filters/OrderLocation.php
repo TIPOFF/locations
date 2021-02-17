c67fd5ab6609f5ee5c4b33f6b32eb17ba0607839
devjk1 <jinsik@omniadigital.io>
2021-02-17 11:51:15 -0500

    [TGER-68] migrate nova filter, OrderLocation

---
+++ src/Nova/Filters/OrderLocation.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class OrderLocation extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * The displayable name of the filter.
+     *
+     * @var string
+     */
+    public $name = 'Location';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->whereHas('order', function ($query) use ($value) {
+            $query->where('orders.location_id', $value);
+        });
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return \App\Models\Location::pluck('id', 'name');
+    }
+}
