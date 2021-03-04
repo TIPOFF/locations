8bf70c53d02c20fa866e193bb698d8aaf79a5fb4
wolfrednicolas <wolfrednicolas@users.noreply.github.com>
2021-03-04 23:40:34 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,8 @@ use Laravel\Nova\NovaCoreServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Locations\LocationsServiceProvider;
-use Tipoff\Support\SupportServiceProvider;
 use Tipoff\Locations\Tests\Support\Providers\NovaPackageServiceProvider;
+use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
 class TestCase extends BaseTestCase
