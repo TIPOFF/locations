1353893ef4b5c3c0c982fc56a184c375382c8e2f
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-23 19:36:06 -0500

    Remove import of deleted class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,10 +10,10 @@ use Spatie\Permission\PermissionServiceProvider;
 use Tipoff\Addresses\AddressesServiceProvider;
 use Tipoff\Authorization\AuthorizationServiceProvider;
 use Tipoff\Locations\LocationsServiceProvider;
-use Tipoff\Locations\Tests\Support\Providers\NovaPackageServiceProvider;
 use Tipoff\Seo\SeoServiceProvider;
 use Tipoff\Support\SupportServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
+use Tipoff\TestSupport\Providers\NovaPackageServiceProvider;
 
 class TestCase extends BaseTestCase
 {
