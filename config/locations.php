f500a1bdba4c239bc8ea8ee0f1bec3cada93eab8
Patrick Breen <patrick.breen@quartzy.com>
2021-03-25 16:30:37 -0400

    Add invalid slugs to config

--- config/locations.php
+++ config/locations.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'invalid_slugs' => ['company', 'about-us'],
+
     'service_class' => [
 
         'calendar' => \App\Services\CalendarService::class
--- tests/Unit/Models/MarketTest.php
+++ tests/Unit/Models/MarketTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tipoff\Locations\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
 use Tipoff\Locations\Models\Market;
 use Tipoff\Locations\Tests\TestCase;
 
@@ -17,5 +18,23 @@ class MarketTest extends TestCase
     {
         $model = Market::factory()->create();
         $this->assertNotNull($model);
+    }
+
+    /** @test */
+    public function default_slug()
+    {
+        $market = Market::factory()->create([
+            'slug' => null,
+        ]);
+        $this->assertEquals(Str::slug($market->city), $market->slug);
+    }
+
+    /** @test */
+    public function restricted_slug()
+    {
+        $market = Market::factory()->create([
+            'slug' => 'company',
+        ]);
+        $this->assertEquals(Str::slug("company-{$market->state}"), $market->slug);
     }
 }
