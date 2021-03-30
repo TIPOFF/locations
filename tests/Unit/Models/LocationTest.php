0df7aebd04bc6c6722db7ab7fbbdf52a7e18a562
Kyle Barney <kyle@kylebarney.dev>
2021-03-29 19:06:27 -0600

    Remove tests for the methods removed in a previous commit

--- tests/Unit/Models/LocationTest.php
+++ tests/Unit/Models/LocationTest.php
@@ -52,42 +52,6 @@ class LocationTest extends TestCase
         $this->assertEquals(0, $location->revenue_booked_last_week);
     }
 
-    /** @test */
-    public function bookings_yesterday_with_service()
-    {
-        $this->stubOrderInterfaceFiltering();
-
-        $location = Location::factory()->create();
-        $this->assertEquals(2, $location->bookings_yesterday);
-    }
-
-    /** @test */
-    public function revenue_booked_yesterday_with_service()
-    {
-        $this->stubOrderInterfaceFiltering();
-
-        $location = Location::factory()->create();
-        $this->assertEquals(35.79, $location->revenue_booked_yesterday);
-    }
-
-    /** @test */
-    public function bookings_last_week_with_service()
-    {
-        $this->stubOrderInterfaceFiltering();
-
-        $location = Location::factory()->create();
-        $this->assertEquals(2, $location->bookings_last_week);
-    }
-
-    /** @test */
-    public function revenue_booked_last_week_with_service()
-    {
-        $this->stubOrderInterfaceFiltering();
-
-        $location = Location::factory()->create();
-        $this->assertEquals(35.79, $location->revenue_booked_last_week);
-    }
-
     private function stubOrderInterfaceFiltering()
     {
         $item1 = \Mockery::mock(OrderItemInterface::class);
