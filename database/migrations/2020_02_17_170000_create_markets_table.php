17658afe428fdb20afd442389de83d336f918f21
Drew Roberts <github@drewroberts.com>
2021-04-01 08:30:19 -0400

    Allow market to extend page for frontend #85 #41

--- database/migrations/2020_02_17_170000_create_markets_table.php
+++ database/migrations/2020_02_17_170000_create_markets_table.php
@@ -12,20 +12,15 @@ class CreateMarketsTable extends Migration
     {
         Schema::create('markets', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->unique()->index();
+            $table->foreignIdFor(app('page')); // Content & slug for website frontend
             $table->string('name')->unique();
             $table->string('title')->unique(); // Market Title for Display. Used publicly.
-            $table->string('state'); // Just the 2-digit abbreviation.
+            $table->foreignIdFor(app('state'));
             $table->foreignIdFor(app('timezone'));
-            $table->text('content')->nullable(); // Market specific content. Written in Markdown.
             $table->date('entered_at'); // Date first location opened in the market.
             $table->date('closed_at')->nullable();
-            $table->string('redirect')->nullable();
-
-            $table->foreignIdFor(app('image'))->nullable(); // Cover image for market
-            $table->foreignIdFor(app('image'), 'ogimage_id')->nullable(); // External open graph image id. Featured image for social sharing. Will default to image_id unless this is used.
+            $table->string('redirect')->nullable(); // May move this to Page in drewroberts/blog
             $table->foreignIdFor(app('image'), 'map_image_id')->nullable(); // Image of location map for the market.
-            $table->foreignIdFor(app('video'))->nullable(); // Featured video for the market.
 
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
