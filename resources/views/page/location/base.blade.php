6c72fb09fccc62500437ff56a2998f7c29544a9d
devjk1 <jinsik@omniadigital.io>
2021-04-29 13:44:43 -0400

    [TGER-179] remove placeholder content; contains unavailable references ->market & ->location

--- resources/views/page/location/base.blade.php
+++ resources/views/page/location/base.blade.php
@@ -1,13 +1,5 @@
 @extends('support::base')
 
 @section('content')
-
-    {{-- Place holder content - safe to replace --}}
-    <ul>
-        <li>Market: {{ $page->market->title }}</li>
-        <li>Page: {{ $page->name }}</li>
-
-        <li>Location: {{ $child_page->location->title }}</li>
-        <li>Child: {{ $child_page->name }}</li>
-    </ul>
+    
 @endsection
