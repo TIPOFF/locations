f2a869d2c05f7079c4a85ca65d379669662d721a
devjk1 <jinsik@omniadigital.io>
2021-04-29 13:47:34 -0400

    [TGER-179] add DomesticAddress & Phone livewire components; used on Pages with Locations Base layout to save DomesticAddress & Phone to db

--- resources/views/page/location/base.blade.php
+++ resources/views/page/location/base.blade.php
@@ -1,5 +1,18 @@
 @extends('support::base')
 
 @section('content')
-    
+<div class="flex flex-col">
+    <div class="w-1/2">
+        @livewire ('addresses::domestic-address-search-bar')
+    </div>
+    <form 
+        method="POST"
+        action="{{ route('demo-submit') }}" 
+        class="w-1/2 my-4"
+    >
+        @csrf
+        @livewire ('addresses::domestic-address-search-bar-fields')
+        @livewire ('addresses::get-phone')
+    </form>
+</div>
 @endsection
