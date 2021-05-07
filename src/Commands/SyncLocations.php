1fc63d006e2b0b2d18379090946683ff6636d5e4
phuclh <phuclh@users.noreply.github.com>
2021-05-07 02:14:00 +0000

    Fix styling

--- src/Commands/SyncLocations.php
+++ src/Commands/SyncLocations.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Locations\Commands;
 
-use Google_Service_MyBusiness;
 use Illuminate\Console\Command;
 use Tipoff\GoogleApi\Facades\GoogleOauth;
 use Tipoff\GoogleApi\GoogleServices;
